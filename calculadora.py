7ca04008adc6343bfcf776185f2f1bdff7297468
Meritxell Diaz Coque <mdiaz@f-l3207-pc49.aulas.etsit.urjc.es>
2020-02-07 14:21:21 +0100

    version 3 calculadora

--- calculadora.py
+++ calculadora.py
@@ -2,19 +2,19 @@
 
 import sys
 
+if len(sys.argv) > 4 or len(sys.argv) != 4:
+    print("Introduce una operacion y un operando")
+    sys.exit(1)
+
 operation = sys.argv[1]
 operand1 =  int(sys.argv[2])
 operand2 =  int(sys.argv[3])
 
 if operation == "add":
     print("El resultado es: "+ str(operand1 + operand2))
-    #llamo a sumar
 if operation == "sub":
     print("El resultado es: "+ str(operand1 - operand2))
-    #llamo a restar
 if operation == "mul":
     print("El resultado es: "+ str(operand1 * operand2))
-    #llamo a mul
 if operation == "div":
     print("El resultado es: "+ str(operand1 / operand2))
-    #llamo a dividir
