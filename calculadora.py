1a7e4aacbbf8ded61954c70506c496eb8d1d25ec
Meritxell Diaz Coque <mdiaz@f-l3207-pc49.aulas.etsit.urjc.es>
2020-02-07 14:12:00 +0100

    version 2 calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,33 +1,20 @@
 #!/usr//bin/python3
 
-
 import sys
 
 operation = sys.argv[1]
 operand1 =  int(sys.argv[2])
 operand2 =  int(sys.argv[3])
 
-def add(operand1, operand2):
-    return operand1 + operand2
-
-def sub(operand1, operand2):
-    return operand1 - operand2
-
-def mul(operand1, operand2):
-    return operand1 * operand2
-
-def div(operand1, operand2):
-    return operand1 / operand2
-
 if operation == "add":
-    print("El resultado es: "+ str(add(operand1, operand2)))
+    print("El resultado es: "+ str(operand1 + operand2))
     #llamo a sumar
 if operation == "sub":
-    print("El resultado es: "+ str(sub(operand1, operand2)))
+    print("El resultado es: "+ str(operand1 - operand2))
     #llamo a restar
 if operation == "mul":
-    print("El resultado es: "+ str(mul(operand1, operand2)))
+    print("El resultado es: "+ str(operand1 * operand2))
     #llamo a mul
 if operation == "div":
-    print("El resultado es: "+ str(div(operand1, operand2)))
+    print("El resultado es: "+ str(operand1 / operand2))
     #llamo a dividir
