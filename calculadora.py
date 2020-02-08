c26f9293e5e1d9431fbc4ef158eec2fbc5a93cb2
meri <mdiaz>
2020-02-08 12:55:15 +0100

    version final con excepciones

--- calculadora.py
+++ calculadora.py
@@ -7,16 +7,23 @@ if len(sys.argv) != 4:
     sys.exit(1)
 
 operation = sys.argv[1]
-operand1 =  int(sys.argv[2])
-operand2 =  int(sys.argv[3])
+try:
+    operand1 =  int(sys.argv[2])
+    operand2 =  int(sys.argv[3])
+except ValueError:
+    print("Introduce un operando valido")
+    sys.exit(1)
 
 if operation == "add":
     result = operand1 + operand2
-if operation == "sub":
+elif operation == "sub":
     result = operand1 - operand2
-if operation == "mul":
+elif operation == "mul":
     result = operand1 * operand2
-if operation == "div":
+elif operation == "div":
     result = operand1 / operand2
+else:
+    print("Introduce una operacion: add, sub, mul o div")
+    sys.exit(1)
 
 print("El resultado es: "+ str(result))
